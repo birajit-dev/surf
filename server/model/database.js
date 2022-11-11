fa5fa35e54d47e8ef2352327fe8de1f5e6dc1b3c
birajit-dev <birajitdebbarma@gmail.com>
2022-11-11 13:56:11 +0530

    database change

--- server/model/database.js
+++ server/model/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect("mongodb://localhost:27017/neheraldnews", {
+mongoose.connect("mongodb://localhost:27017/neherald", {
    useNewUrlParser: true,
    useUnifiedTopology: true
 });
